290588f63c746a0f7d03621b89b04fa6759ec9f7
ljh <ljh@ljh.studio>
2022-08-06 17:48:48 +0900

    Add set

--- jump_to_python/01.py
+++ jump_to_python/01.py
@@ -1,3 +1,19 @@
+import time
 
+COUNT = 50_000
 
-print()
+# list
+t0 = time.time()
+target_list = list(range(COUNT))
+for i in range(COUNT):
+    if i in target_list:
+        pass
+print("list : ", time.time() - t0)
+
+# set
+t1 = time.time()
+target_set = set(range(COUNT))
+for i in range(COUNT):
+    if i in target_set:
+        pass
+print("set : ", time.time() - t1)
